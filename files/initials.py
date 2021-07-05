34f1e81e24b0b17eedc3d67026f0cb3949635457
Lane2002 <woodelaine2002@hotmail.co.uk>
2021-07-05 14:03:46 +0100

    first commit

---
+++ files/initials.py
@@ -0,0 +1,39 @@
+# Elaine W
+
+#Fun Fact: I love swimming.
+
+print("EEEEE")
+print("E")
+print("E")
+print("EEE")
+print("E")
+print("E")
+print("EEEEE")
+
+name = """
+W    W
+W    W
+W    W
+W  W W
+WW  WW
+W    W
+"""
+print(name)
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
